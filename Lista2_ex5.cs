811330b981f38ed6febc3000f4368cf4230fe277
amykrisley <130614797+amykrisley@users.noreply.github.com>
2023-05-03 18:10:58 -0300

    Add files via upload

---
+++ Lista2_ex5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, h, a;
+
+            Console.Write("Digite a base:");
+            b = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura:");
+            h = double.Parse(Console.ReadLine());
+
+            a = b * h;
+
+            Console.WriteLine("área = {0}", a);
+
+            if (a > 100)
+                Console.WriteLine("terreno grande");
+            else
+                Console.WriteLine("terreno pequeno");
+        }
+    }
+}
